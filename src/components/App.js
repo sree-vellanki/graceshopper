7c6f67856a3c12154b8498c9f8f9392c0731ccba
Amber Manley <a.manley1556@gmail.com>
2020-07-09 19:30:06 -0400

    updating files

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import {
-  getSomething
-} from '../api';
+import { getSomething } from "../api";
+
+import "./App.css";
 
 const App = () => {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     getSomething()
-      .then(response => {
+      .then((response) => {
         setMessage(response.message);
       })
-      .catch(error => {
+      .catch((error) => {
         setMessage(error.message);
       });
   });
 
-  return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-      <h2>{ message }</h2>
-    </div>
-  );
-}
+  //   function handleSearch(event) {
+  //     setSearch(event.target.value);
+  //   }
+
+  //   return (
+  //     <div className="App">
+  //       <h1>Grace Shopper Project...</h1>
+
+  //       <div id="searchBox">
+  //         <form>
+  //           <input
+  //             type="text"
+  //             onChange={handleSearch}
+  //             placeholder="Search here..."
+  //             value={search}
+  //             name="search"
+  //           />
+  //         </form>
+  //       </div>
+  //       <h2>{message}</h2>
+  //     </div>
+  //   );
+};
 
 export default App;
