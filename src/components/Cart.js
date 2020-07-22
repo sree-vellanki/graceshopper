f9447b6a47645e5f470aeadfbd9341625bef2472
Cherrie <cherrie.alfonso@gmail.com>
2020-07-21 21:25:27 -0400

    working on cart modal

---
+++ src/components/Cart.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import {Button, Modal, Tabs, Tab, Form} from "react-bootstrap";
+
+const CartButton = () => {
+    return (
+        <div>
+            <Button>cart</Button>
+        </div>
+    )
+}
--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -4,9 +4,88 @@ import "./TopNavigation.css";
 
 const TopNavigation = () => {
   const [navigation, setNavigation] = useState("");
+<<<<<<< Updated upstream
 
   function handleNavigation(event) {
     setNavigation(event.target.value);
+=======
+  const [username, setUsername] = ("");
+  const [password, setPassword] = ("");
+
+  const showModal = () => {
+    setIsOpen(true);
+  };
+  const hideModal = () => {
+    setIsOpen(false);
+  };
+
+  // useEffect(() => {
+  //   axios.post((req) => {
+
+  //   })
+  // })
+
+  // function handleNavigation(event) {
+  //   event.preventDefault()
+  //   setNavigation(event.target.value);
+  // }
+
+  const LoginRegisterModal = () => {
+    return(
+      <Modal
+        size="sm"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={isOpen}
+        onHide={hideModal}
+        className="log-reg-modal"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Tabs defaultActiveKey="login" className="tabs">
+          <Tab eventKey="login" title="Log In">
+            <Form>
+              <Form.Group controlId="formLoginUsername" value={username}>
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="username" placeholder="Username" />
+              </Form.Group>
+              <Form.Group controlId="formLoginPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" value={password} />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Tab>
+          <Tab eventKey="register" title="Register">
+            <Form>
+              <Form.Group controlId="formRegUsername">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+              <Form.Group controlId="formRegPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group controlId="formRegPasswordConfirm">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="confirm-password" placeholder="Confirm password" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Tab>
+        </Tabs>
+        </Modal.Body>
+      </Modal>
+    )
+>>>>>>> Stashed changes
   }
 
   return (
